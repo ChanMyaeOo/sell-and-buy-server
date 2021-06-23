d35dca67b32b1b7189b98be192cabff6bfdb863e
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-06-23 18:08:06 +0630

    added patch route

--- controllers/ItemsController.js
+++ controllers/ItemsController.js
@@ -1,16 +1,16 @@
 import mongoose from 'mongoose';
 import ItemModel from '../models/items.js'
 
-export const getItems = async (req, res) => { 
+export const getItems = async (req, res) => {
     try {
         const items = await ItemModel.find()
         res.send(items)
     } catch (error) {
-        res.status(404).json({ message: error.message})
+        res.status(404).json({ message: error.message })
     }
 }
 
-export const createItem = async (req, res) => { 
+export const createItem = async (req, res) => {
     const item = req.body;
     const newItem = new ItemModel({ ...item, createdAt: new Date().toISOString() });
 
@@ -20,4 +20,13 @@ export const createItem = async (req, res) => {
     } catch (error) {
         res.status(409).json({ message: error.message })
     }
+}
+
+export const updateItem = async (req, res) => {
+    const { id: _id } = req.params;
+    const item = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).json({ message: "No item found with that ID" })
+    const updatedItem = await ItemModel.findByIdAndUpdate(_id, { ...item, _id }, { new: true })
+    res.status(201).json(updatedItem)
 }
--- routes/ItemsRoute.js
+++ routes/ItemsRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getItems, createItem} from '../controllers/ItemsController.js';
+import { getItems, createItem, updateItem } from '../controllers/ItemsController.js';
 
 const router = express.Router();
 
 router.get('/', getItems)
 router.post('/createItem', createItem)
+router.patch('/:id', updateItem)
 
 export default router;
